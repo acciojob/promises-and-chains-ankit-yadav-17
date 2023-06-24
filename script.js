b812554b36d4948dc2718500055e832560580879
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-06-24 13:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const age = document.getElementById("age").value
+const name = document.getElementById("name").value
+const btn = document.getElementById("btn");
+btn.addEventListener('submit',(event) => {
+	event.preventDefault();
+})
+
+var promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+          if (parseInt(age) >= 18) {
+            resolve(name);
+          } else {
+            reject(name);
+          }
+        }, 4000);
+      });
+
+ promise.then(function(name) {
+        alert(`Welcome, ${name}. You can vote.`);
+      }).catch(function(name) {
+        alert(`Oh sorry ${name}. You aren't old enough.`);
+      });
